95bfdde98ff1be909a8df681e0e2930638562023
Manoj <pmontu@gmail.com>
2014-08-21 12:45:38 +0530

    Student Receipts in Progress: Student select

--- js/vlcia.js
+++ js/vlcia.js
@@ -1,30 +1,16 @@
-var app = angular.module("vlcimApp",['ui.bootstrap','ngRoute','ngCookies']);
+var app = angular.module("vlcimApp",['ui.bootstrap','ngRoute']);
 
 //app.controller = DatepickerDemoCtrl;
 
 app.config(function($routeProvider,$httpProvider){
   $routeProvider.when('/',
   {
-    controller: "searchController",
-    templateUrl: "views/searchandreports.html"
+    controller: "receiptController",
+    templateUrl: "views/receipts.html"
   })
-  .when('/searchandreports',
-  {
-    controller: "searchController",
-    templateUrl: "views/searchandreports.html"
-  })
-  .when('/addnewstudent',
-  {
-    templateUrl: "views/addnewstudent.html"
-  })
-  .when('/addnewfaculty',
-  {
-    templateUrl: "views/addnewfaculty.html"
-  })
-  .when('/searchfaculty',
-  {
-    controller:'searchFacultyController',
-    templateUrl:'views/searchfaculty.html'
+  .when('/receipt',{
+    templateUrl:"views/receipts.html",
+    controller:"receiptController"
   });
   $httpProvider.defaults.transformRequest = function(data){
     if (data === undefined) {
@@ -34,75 +20,52 @@ app.config(function($routeProvider,$httpProvider){
   }
 });
 
-var DatepickerDemoCtrl = function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+app.controller("receiptController",function($scope,instituteFactory){
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+  //  STAGE 1 - STUDENTS
+  var refreshStudents = function(){
 
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
+    //  RESET
+    $scope.filterStudent = null;
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
+    //  VALIDATION
+    var requestData = {
+      "name":$scope.searchStudentForm.name,
+      "id":$scope.searchStudentForm.id
+    }
 
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
+    //  FETCH STUDENTS
+    instituteFactory.getStudents(requestData).success(function(data){
+      if(data.length>0)
+        $scope.students = data;
+    });
   };
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
+  //  STAGE 2 - RECEIPTS
+  var refreshReceipts = function(student){
+    $scope.filterStudent = student;
   };
 
-  $scope.initDate = new Date('2016-15-20');
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
-};
+  $scope.refreshStudents = refreshStudents;
+  $scope.refreshReceipts = refreshReceipts;
 
-app.controller("searchController", function($scope){
-  $scope.search = {
-    faculty:{
-      area:""
-    },
-    student:{}
-  };
-  $scope.result = [
-    {type:"Student",roll:2014177,name:"manoj",area:"vadapalani",overdue:1000},
-    {type:"Student",roll:2014178,name:"ajay",area:"vadapalani",overdue:10000},
-    {type:"Student",roll:2014179,name:"radhika",area:"kodambakkam",overdue:5000},
-    {type:"Student",roll:2014180,name:"abhi",area:"kodambakkam",overdue:0},
-    {type:"Faculty",roll:1,name:"akshay",area:"vadapalani",overdue:1000},
-    {type:"Faculty",roll:2,name:"arjun",area:"kodambakkam",overdue:20000},
-    {type:"Faculty",roll:3,name:"ram",area:"vadapalani",overdue:0},
-    {type:"Faculty",roll:4,name:"sita",area:"kodambakkam",overdue:0},
-  ];
+  //  INITIALISE
+  //refreshStudents();
 
 });
 
-app.controller("searchFacultyController",function($scope, instituteFactory){
-  $scope.circle="VAD";
-  instituteFactory.getFaculty("Area=VAD").success(function(data){
-    $scope.result = data;
-  });
-});
 
-app.factory('instituteFactory', function($http, $cookies){
+app.factory('instituteFactory', function($http){
+
   var factory = {};
-  factory.getFaculty = function (search){
-    return $http.post('http://127.0.0.1:12346/institute/');
-    console.log(search)
-  };
+  var domain = "http://127.0.0.1:12346/institute/";
+
+  //$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+  factory.getStudents = function(search){
+    return $http.post(domain+"student/",search);
+  }
+
   return factory;
 });
